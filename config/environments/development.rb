86cb017593f42efb4020e1ad122644a2c60cafc8
Davi Mendes <davi26030@gmail.com>
2023-09-06 15:41:52 -0300

    ENV

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     user_name: 'schopz.social@gmail.com',
-    password: PASSWD,
+    password: ENV['PASSWD'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     user_name: 'schopz.social@gmail.com',
-    password: PASSWD,
+    password: ENV['PASSWD'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
